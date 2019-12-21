60dbf50b16f6028d367c94efc0aaa7bfd442b799
SaySiThu <sithusan.codex@gmail.com>
2019-12-22 00:17:41 +0630

    Database Relationships

--- app/SubSubCategory.php
+++ app/SubSubCategory.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class SubSubCategory extends Model
 {
     //
+    public function subCategory(){
+        return $this->belongsTo('App\SubCategory');
+    }
+    public function article(){
+        return $this->hasMany('App\Article');
+    }
 }
